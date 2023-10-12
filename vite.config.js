f6a865cd36c94b43f50afe6b6664acf25edbe07b
Horace HOU <HoraceHou@CoatesGroup.com>
2023-10-12 21:51:53 +1100

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const port = 3000;
-// const port = process.env.PORT || 3000;
+// const port = 3000;
+const port = process.env.PORT || 3000;
 
 // https://vitejs.dev/config/
 export default defineConfig({
